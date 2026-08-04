cdf34075010af643ffeae83994ef1431f4fb1206
Aditi Suryavanshi <aditi222390@gmail.com>
2026-08-04 10:01:28 +0000

    add this file 110_sort.js

---
+++ 110_sort.js
@@ -0,0 +1,11 @@
+// sort() Method
+
+let numbers = [50, 10, 40, 20, 30];
+
+console.log("Before Sort:");
+console.log(numbers);
+
+numbers.sort((a, b) => a - b);
+
+console.log("After Sort:");
+console.log(numbers);
