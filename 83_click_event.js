d8154ee8ef0bf53dde5d80c343372ad0f64392ff
Aditi Suryavanshi <aditi222390@gmail.com>
2026-08-04 09:16:47 +0000

    add this file 83_click_event.js

---
+++ 83_click_event.js
@@ -0,0 +1,9 @@
+<button id="btn">Click</button>
+
+<script>
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+    console.log("Button clicked");
+});
+</script>
