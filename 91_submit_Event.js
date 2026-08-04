9ed2fde6582fb07425c3edef0d707cf8f4bc1f5f
Aditi Suryavanshi <aditi222390@gmail.com>
2026-08-04 09:26:25 +0000

    add this file 91_submitEvent.js

---
+++ 91_submit_Event.js
@@ -0,0 +1,13 @@
+<form id="form">
+    <input type="text">
+    <button>Submit</button>
+</form>
+
+<script>
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("Form Submitted");
+});
+</script>
