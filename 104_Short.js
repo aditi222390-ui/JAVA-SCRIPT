4c4eba4a8aa91b5063c7c6420c2d251a83f26b67
Aditi Suryavanshi <aditi222390@gmail.com>
2026-08-04 09:55:25 +0000

    add this file 104_Short.js

---
+++ 104_Short.js
@@ -0,0 +1,11 @@
+
+
+let numbers = [50, 10, 40, 20, 30];
+
+console.log("Original Array:");
+console.log(numbers);
+
+numbers.sort((a, b) => a - b);
+
+console.log("Sorted Array (Ascending):");
+console.log(numbers);
